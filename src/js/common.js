d9d539fb1e7422ff79d8adba986355a0561ab6d0
gordondalos <gordondalos@gmail.com>
2015-12-25 17:46:05 +0600

    переделал логику отрисовки меню и выпадения, нужно сделать на главной поиск

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
 
+
+
+
+
+
     // поиск верхний и нижний  и для маленького меню
-
-
 
     $('.apps_sm').click(function(e){
         e.preventDefault();
@@ -98,11 +101,11 @@ $(document).ready(function () {
             var drop_menu_large = $('.drop_menu_large').scrollTop();
 
             if(posTop>drop_menu_large){
-                posTop = posTop-79;
+                posTop = posTop;
                 $('.drop_menu_large').css("margin-top",posTop);
 
             }else{
-                $('.drop_menu_large').css("margin-top","-79px");
+                $('.drop_menu_large').css("margin-top","0");
             }
 
         }
@@ -120,19 +123,72 @@ $(document).ready(function () {
         $('.app i').toggleClass('blak_nav');
         $('.drop_menu').slideToggle('fast');
 
+
+
     });
 
+
+    // стикер для горизонтального меню
+    $("#sticker").sticky({topSpacing:0});
+
+    // выпадение меню на больших экранах селективно к главной странице
 
     $('.down').click(function(e){
         e.preventDefault();
-        $(this).toggleClass('blak_nav');
-        $('.down i').toggleClass('blak_nav');
-        $('.drop_menu_down').slideToggle('fast');
+        var main_page = $("#sticker").parent().parent().hasClass("dubble_menu");
+
+        if(!main_page){
+            $(this).toggleClass('blak_nav');
+            $('.down i').toggleClass('blak_nav');
+            $('.drop_menu_down').slideToggle('fast');
+            var left_position_navbar = $(this).position().left;
+            var top_position_navbar = $(this).position().top;
+            var width_navbar = $(this).width();
+            var heigth_navbar = $(this).height();
+            var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+            $(".drop_menu_down").offset({top:posTop+heigth_navbar, left:left_position_navbar-167});
+        }else{
+            $('.down').toggleClass('blak_nav');
+            $('.down i').toggleClass('blak_nav');
+            $('.drop_menu_down').slideToggle('fast');
+
+            var top_position_min = $("#sticker").parent().parent().position().top;
+            var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+
+
+            if(posTop < top_position_min){
+                var top_position_navbar = top_position_min;
+                var left_position_navbar = $(this).position().left;
+                var heigth_navbar = $(this).height();
+                $(".drop_menu_down").offset({top:top_position_navbar+heigth_navbar, left:left_position_navbar-167});
+            }else{
+                var left_position_navbar = $(this).position().left;
+                var heigth_navbar = $(this).height();
+                $(".drop_menu_down").offset({top:posTop+heigth_navbar, left:left_position_navbar-167});
+            }
+        }
+
+
 
     });
 
 
 
+    window.onscroll = function() {
+
+        var top_position_min = $("#sticker").parent().parent().position().top;
+        var posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        var heigth_navbar = $('.down').height();
+        var left_position_navbar = $('.down').position().left;
+        console.log(posTop);
+        if(posTop < top_position_min){
+            $(".drop_menu_down").offset({top:top_position_min+heigth_navbar, left:left_position_navbar-167});
+        }else{
+            $(".drop_menu_down").offset({top:posTop+heigth_navbar, left:left_position_navbar-167});
+        }
+
+    };
 
 
 
@@ -219,6 +275,8 @@ $(document).ready(function () {
         $(this).parent().children("a").trigger('click');
 
     });
+
+
 
     $(".panel-title a").click(function(e){
         e.preventDefault();
